79cb22993fc7039b6702424713bdf3f56f357251
S.M.Imtiaz <roadwheel11@outlook.com>
2017-09-17 06:27:42 +0600

    Upload Image with Image Validation

--- index.php
+++ index.php
@@ -23,16 +23,24 @@ if(isset($_POST['submit'])){
 	$img_uname = substr(md5(time()),0,10).'.'.$img_ext;
 	$img_fname = "uploads/".$img_uname;
 	
-	move_uploaded_file($img_tmp_name,$img_fname);
-	
-	$query = "INSERT INTO tbl_img(image) VALUES('$img_fname')";
-	$upload = $db->upload($query);
-	if($upload){
-		echo "<span class='success'>Image Uploaded Successfullly</span>";
-	}else{
-		echo "<span class='error'>Image not Uploaded</span>";
+	//upload with image validation.
+	if(empty($img_fname)){
+		echo "<span class = 'error'>Please Select an Image !</span>";
+	}else if($img_size>512000){
+		echo "<span class = 'error'>Image size Should be leass then 500KB !</span>";
+	}else if(in_array($img_ext,$permited)==false){
+		echo "<span class = 'error'>You can upload only:". implode(',', $permited) ."</span>";
+	}else{	
+		move_uploaded_file($img_tmp_name,$img_fname);
+		
+		$query = "INSERT INTO tbl_img(image) VALUES('$img_fname')";
+		$upload = $db->upload($query);
+		if($upload){
+			echo "<span class='success'>Image Uploaded Successfullly</span>";
+		}else{
+			echo "<span class='error'>Image not Uploaded</span>";
+		}
 	}
-	
 	
 }
 ?>
